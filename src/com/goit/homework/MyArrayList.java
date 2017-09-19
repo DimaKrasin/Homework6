4440cb69e60f1578e711180f4f7fbd7e22024dba
DimaKrasin <ktasindim@gmail.com>
2017-09-19 20:59:29 +0300

    add part 2

--- src/com/goit/homework/MyArrayList.java
+++ src/com/goit/homework/MyArrayList.java
@@ -31,14 +31,18 @@ public class MyArrayList<T> {
     }
 
     public void remove(int index){
-        Object[] tmp = new Object[size-2];
-        System.arraycopy(Arr, 0, tmp, 0, Arr.length-index+1);
-        if(index!=Arr.length-1){
-            System.arraycopy(Arr, index+1, tmp, index, Arr.length-(Arr.length-index));}
 
-        Arr = (T[]) new Object[size-2];//1
-        System.arraycopy(tmp, 0, Arr, 0, tmp.length);
-        size = size-1;
+        if (index<Arr.length&&index>=0) {
+            Object[] tmp = new Object[size - 2];
+            System.arraycopy(Arr, 0, tmp, 0, Arr.length-(Arr.length-index));
+            if (index != Arr.length - 1) {
+                System.arraycopy(Arr, index + 1, tmp, index, Arr.length - (index+1));
+            }
+
+            Arr = (T[]) new Object[size - 2];//1
+            System.arraycopy(tmp, 0, Arr, 0, tmp.length);
+            size = size - 1;
+        }else{System.out.println("Неверный индекс");}
     }
 
     public void show() {
